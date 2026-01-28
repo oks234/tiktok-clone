7205c92b13e79980f73d5ac07991a5965864f25e
Kyungseok <bestspx@gmail.com>
2026-01-28 17:05:55 +0900

    #3.0 Initialization

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primaryColor: Color(0xFFE9435A)),
+      home: Container(),
+    );
+  }
+}
